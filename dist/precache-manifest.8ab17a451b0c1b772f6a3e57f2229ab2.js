4defcf56457c66dacfc40802ba33b0a7c27373b5
Zh <zeming@dreamofsky.com>
2020-07-14 16:40:50 -0400

    commit dist for gh-pages

---
+++ dist/precache-manifest.8ab17a451b0c1b772f6a3e57f2229ab2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29a0d56f94ebe071a2c5b0fc22085a61",
+    "url": "/task-man/404.html"
+  },
+  {
+    "revision": "fad3ecfc247275975e68",
+    "url": "/task-man/css/app.9c5a7001.css"
+  },
+  {
+    "revision": "f7a59e98a533717144e3",
+    "url": "/task-man/css/chunk-vendors.13f94d68.css"
+  },
+  {
+    "revision": "709eccd160fb28b538b892b0bde502aa",
+    "url": "/task-man/form.png"
+  },
+  {
+    "revision": "0661f38ca1bc44bd3477cd28d69dd2ce",
+    "url": "/task-man/index.html"
+  },
+  {
+    "revision": "fad3ecfc247275975e68",
+    "url": "/task-man/js/app.056b69f7.js"
+  },
+  {
+    "revision": "c75e719b0c9b0f7aac94",
+    "url": "/task-man/js/chunk-firestore.a388e6f1.js"
+  },
+  {
+    "revision": "f7a59e98a533717144e3",
+    "url": "/task-man/js/chunk-vendors.d432a46d.js"
+  },
+  {
+    "revision": "382f7bc9f2d7acabd2facfa30bd2469b",
+    "url": "/task-man/logo.png"
+  },
+  {
+    "revision": "134044fa07d64dbc5f2c2f01575293e4",
+    "url": "/task-man/main.jpg"
+  },
+  {
+    "revision": "4799abd40886c49f4452418775d9d2c2",
+    "url": "/task-man/manifest.json"
+  },
+  {
+    "revision": "c090d00c9aef765237f50c25f5f87383",
+    "url": "/task-man/mix.jpg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/task-man/robots.txt"
+  }
+]);
